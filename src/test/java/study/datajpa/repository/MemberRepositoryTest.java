afa45db806b2b97dcd7fbb78973d212939268c59
YunhoJung <saijdu2198@gmail.com>
2021-12-20 23:16:30 +0900

    test: 프로젝션 테스트 코드 추가

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -308,4 +308,45 @@ public class MemberRepositoryTest {
 
         Assertions.assertThat(result.get(0).getUsername()).isEqualTo("m1");
     }
+
+    @Test
+    public void projections(){
+        // given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        // when
+//        List<UsernameOnly> result = memberRepository.findProjectionsByUsername("m1");
+//
+//        for (UsernameOnly usernameOnly : result) {
+//            System.out.println("usernameOnly = " + usernameOnly);
+//        }
+
+//        List<UsernameOnlyDto> usernameOnlyDtos = memberRepository.findProjectionsByUsername("m1");
+//
+//        for (UsernameOnlyDto usernameOnlyDto : usernameOnlyDtos) {
+//            System.out.println("usernameOnlyDto = " + usernameOnlyDto.getUsername());
+//        }
+
+        List<NestedClosedProjections> nestedClosedProjections = memberRepository.findProjectionsByUsername("m1", NestedClosedProjections.class);
+
+        for (NestedClosedProjections nestedClosedProjection : nestedClosedProjections) {
+            String username = nestedClosedProjection.getUsername();
+            System.out.println("username = " + username);
+            String teamName = nestedClosedProjection.getTeam().getName();
+            System.out.println("teamName" + teamName);
+        }
+
+
+
+
+    }
 }
